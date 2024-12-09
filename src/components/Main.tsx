b41b5fc54e7eda6e1dd7816eaab51600933f25fe
ronenram1234 <60760101+ronenram1234@users.noreply.github.com>
2024-12-09 12:51:31 +0200

    design cards and carousel

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,17 +1,19 @@
-import { FunctionComponent, useContext, useEffect } from "react";
+import { FunctionComponent, useContext, useEffect, useState } from "react";
 import { GlobalProps } from "../App";
-// import { useNavigate } from "react-router-dom";
+
 import ModalLoginReg from "./ModalLoginReg";
 import { getAllCards } from "../services/cardServices";
 import { errorMsg } from "../services/feedbackService";
-import { RouteRounded } from "@mui/icons-material";
+
 import CraeteCard from "./CraeteCard";
+import { Carousel, Row, Tab, Tabs } from "react-bootstrap";
 
 interface MainProps {}
 
 const Main: FunctionComponent<MainProps> = () => {
   const { isDarkMode, isUserLogedin, cardArray, setCardArray } =
     useContext(GlobalProps);
+    const [activeTab, setActiveTab] = useState<string>("Tab 1");
   // const navigate = useNavigate();
   // console.log(token);
 
@@ -34,22 +36,43 @@ const Main: FunctionComponent<MainProps> = () => {
     getAllCardsFromAPI();
   }, []);
 
+   // Split cards into chunks for tabs
+   const chunkCards = (cards: any[], chunkSize: number) => {
+    const chunks = [];
+    for (let i = 0; i < cards.length; i += chunkSize) {
+      chunks.push(cards.slice(i, i + chunkSize));
+    }
+    return chunks;
+  };
+
+  const cardChunks = chunkCards(cardArray || [], 10); // 15 cards per tab
+
   return (
-    <>
-      {/* Use Fragment to return the list of cards */}
-      <>
-        {cardArray?.map((item, ind) => {
-          if (ind < 2) {
-            
-           return  <CraeteCard item={item} ind={ind}/>
-            
-          }
-          return null;
-        })}
-      </>
-      {/* <h1>main</h1> */}
-      {!isUserLogedin && (<ModalLoginReg />)}
-    </>
+    <div className="container mt-4">
+    <Tabs
+      id="card-tabs"
+      activeKey={activeTab}
+      onSelect={(k) => setActiveTab(k || "Tab 1")}
+      className="mb-3"
+    >
+      {cardChunks.map((chunk, index) => (
+        <Tab eventKey={`Tab ${index + 1}`} title={`Tab ${index + 1}`} key={index}>
+          <Carousel interval={null}>
+            {chunk.map((item, ind) => (
+              <Carousel.Item key={ind}>
+                <Row xs={1} sm={2} md={3} lg={4} className="g-4">
+                  {chunk.map((card, cardIndex) => (
+                    <CraeteCard item={card} ind={cardIndex} key={cardIndex} />
+                  ))}
+                </Row>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </Tab>
+      ))}
+    </Tabs>
+    {!isUserLogedin && <ModalLoginReg />}
+  </div>
   );
 };
 
